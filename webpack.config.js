19e65bdfe60d7f351009bca015bbc38fa03d5121
Ivan <garcia.ballesteros96@gmail.com>
2021-04-20 13:29:20 +0200

    commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(c|sc|sa)ss$/,
+                exclude: /styles\.(c|sc|sa)ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /styles\.(c|sc|sa)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.[contenthash].css'
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.[contenthash].html',
+        }),
+    ]
+}
